bb959fb218074c32d6ea901516eb8c4e22cb4920
nicefeiniu <872938058@qq.com>
2018-06-28 18:31:05 +0800

    test

--- translation_web/translation_web/settings.py
+++ translation_web/translation_web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'r#^1d(*6r2*gr*-0-pvtboof6=c@1r@op61ld&ohqfkto5w4ov'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
